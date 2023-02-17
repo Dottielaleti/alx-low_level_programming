01552db2b6d3b417aa3af9153c11a7d2776f8ea9
Dottielaleti <laletidottie@gmail.com>
2023-02-16 17:13:13 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - A program that print a line using print function
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	printf("With proper grammar, but the outcome is place of art, \n*)
+        return (0);
+}
