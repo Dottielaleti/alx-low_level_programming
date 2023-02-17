a0d188c4e3d01f106f7a2a0f1ddbbf7a9012dd54
Dottielaleti <laletidottie@gmail.com>
2023-02-17 13:49:58 -0800

    double digit combinations

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - print different two digit combinations
+ *
+ * Return: always 0
+ *
+ */
+int main(void)
+{
+	int num1, num2;
+
+
+	for (num1 = 0; num1 < 9; num1++)
+	{
+		for (num2 = 0; num2 < 10; num2++)
+		{
+			if (num1 != num2)
+			{
+				putchar(num1 + '0');
+				putchar(num2 + '0');
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
