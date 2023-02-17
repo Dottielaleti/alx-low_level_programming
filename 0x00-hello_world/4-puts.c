539fea979b40733b5425ab557d652913cdd11ca5
Dottielaleti <laletidottie@gmail.com>
2023-02-16 17:00:41 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,13 @@
-#!/bin/bash
-nuoajkas
+##include <stdio.h>
+
+/**
+ * main - A program that prints a line with puts functions
+ n
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
