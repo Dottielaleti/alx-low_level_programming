2f6f8ee00e9164cc70cffb35839360c694fead35
Dottielaleti <laletidottie@gmail.com>
2023-02-17 15:08:26 -0800

    triple digit combination

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - three digit combinaton
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int num1, num2, num3;
+
+	for (num1 = 0; num1 < 10; num1++)
+	{
+		for (num2 = 0; num2 < 10; num2++)
+		{
+			for (num3 = 0; num3 < 10; num3++)
+			{
+				if (num1 != num2 != num3 && num1 != num2 && num2 != num3)
+				{
+					if (num1 < num2 < num3 && num1 < num2 && num2 < num3)
+					{
+						if (num1 < 7 && num2 < 9 && num3 < 10)
+						{
+							putchar(',');
+							putchar(' ');
+						}
+						putchar(num1 + '0');
+						putchar(num2 + '0');
+						putchar(num3 + '0');
+					}
+				}
+
+			}
+
+		}
+
+	}
+	putchar('\n');
+}
