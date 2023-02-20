29a6940d3af42e4bf29c871ca2b71653cb668f8c
Dottielaleti <laletidottie@gmail.com>
2023-02-20 04:51:59 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main-program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19
+ *
+ * Return- always 1
+ *
+ */
+int main (void)
+{    write ("and that piece of art is useful" - Dora Korpar, 2015-10-19\n")
+     return (1);	
+}
