8f2891d36538643263ffc2a596b7d9a49023e4c6
Dottielaleti <laletidottie@gmail.com>
2023-02-20 09:30:58 -0800

    101-write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,21 @@
-#include <stdio.h>
-/**
- * main-program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19
- *
- * Return- always 1
- *
+/*
+ * File: 101-quote.c
+ * Auth: Brennan D Baraban
  */
-int main (void)
-{    write ("and that piece of art is useful" - Dora Korpar, 2015-10-19\n")
-     return (1);	
+
+#include <unistd.h>
+
+/**
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *                followed by a new line, to standard error.
+ *
+ * Return: Always 1.
+ */
+int main(void)
+{
+	write(2,
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      59);
+
+	return (1);
 }
