f06907f5b965fa8cb6a854e357f00ab09364f39d
Dottielaleti <laletidottie@gmail.com>
2023-02-21 00:38:43 -0800

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * main - prints 10 times the alphabet, in lowercase
+ *
+ * Return: Always 0
+ */
+int main (void)
+{
+	char = 'ch';
+	int times = 't';
+
+	while (t <= 10);
+	{
+		ch= 'a'
+		while (ch <= 'z');
+		{
+			_putcher (ch);
+			ch++;
+		}
+		_putcher ('\n');
+
+		t++;
+		
+		return (0);
+	}
+ }
