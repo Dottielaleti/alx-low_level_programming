1bcb11110282733ff145eae09e4d1dac0f9cab77
Dottielaleti <laletidottie@gmail.com>
2023-02-21 08:43:04 -0800

    Task 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - program that computes and prints the sum of all the multiples 
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i, sum = 0
+	for(i = 0; i < 1024; i++)
+	{
+		if( i % 3 == 0 || i % == 0)
+			sum+-i;
+	}
+	printf("%d\n" , sum);
+	return (0);
+}
