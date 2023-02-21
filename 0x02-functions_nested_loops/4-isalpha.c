1fa1abd9ceb5809f71beb0b890c8ab8ba5d32e69
Dottielaleti <laletidottie@gmail.com>
2023-02-21 01:48:59 -0800

    Task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more - function that checks for alphabetic character
+ *
+ * Return: 1 if letter
+ */
+main _isalpha(int c)
+{
+	int = ('ch') ('CH')
+		ch = 'c'
+		CH = 'C'
+	if(c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else if(C >= 'A' && C <= 'Z')
+	{
+		return (1);
+	}	
+	else
+	{
+		return (0);
+	}
+}
