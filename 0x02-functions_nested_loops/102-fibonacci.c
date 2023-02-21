bf600becb70b14c48edfa806895f6db62545c21a
Dottielaleti <laletidottie@gmail.com>
2023-02-21 09:32:23 -0800

    task 14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * more - program that prints the first 50 Fibonacci numbers
+ *
+ * Return: Always 0
+ */
+main int(void)
+{
+	long pri = 1, sec = 2, sum;
+	printf ("1 , 2")
+	while (sum < 20365011073)
+	{
+		sum = pri + secs;
+		printf(", %lu" , sum);
+		pri = sec;
+		sec = sum;
+
+	}
+	printf("\n");
+	return (0);
+}
