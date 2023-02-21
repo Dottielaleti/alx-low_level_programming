754fd5559549ccea55da8d0908ee721453221aa1
Dottielaleti <laletidottie@gmail.com>
2023-02-21 05:49:05 -0800

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - function that adds two integers and returns the result
+ *
+ * Return: Always 0.
+ */
+main int(add int x , int y)
+{
+	return (x + y)
+}
