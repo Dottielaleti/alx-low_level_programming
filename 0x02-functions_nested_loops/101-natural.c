2927e4d2aa2cebe2360e737ff3d61746802b66e8
Dottielaleti <laletidottie@gmail.com>
2023-02-21 09:06:49 -0800

    task 13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,10 +8,10 @@
 int main(void)
 {
 	int i, sum = 0
-	for(i = 0; i < 1024; i++)
+	for (i = 0; i < 1024; i++)
 	{
-		if( i % 3 == 0 || i % == 0)
-			sum+-i;
+		if ( i % 3 == 0 || i % == 0)
+			sum += n;
 	}
 	printf("%d\n" , sum);
 	return (0);
