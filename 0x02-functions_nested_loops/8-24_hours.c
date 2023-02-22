d6e070489967a91fdc2de2b7d08b2b1a628f4a87
Dottielaleti <laletidottie@gmail.com>
2023-02-22 06:35:22 -0800

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,9 +10,9 @@ void jack_bauer(void)
 {
 	int hour, minute;
 
-        for (hour = 0; && hour <= 23; hour++)
+        if (hour = 0; && hour <= 23; hour++)
 
-	for (minute = 0; && minute <= 59; minute++)
+	if (minute = 0; && minute <= 59; minute++)
 	{
 		putchar (hour / 10 + '0');
 		putchar (hour % 10 + '0');
