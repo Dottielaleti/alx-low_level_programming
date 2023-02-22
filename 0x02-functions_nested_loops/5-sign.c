f3cdebd99f626223d0955c8f20d00e154294a9f2
Dottielaleti <laletidottie@gmail.com>
2023-02-22 04:28:04 -0800

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,24 +2,25 @@
 
 /**
  * print_sign -  function that prints the sign of a number
+ * @n: The number in which the sign will be printed.
  *
  *Return: 1 if n > 0 , 0 if n = 0 , -1 if n < 0
  */
 int print_sign(int n)
 {
-	if( n > 0)
+	if (n > 0)
 	{
-		_putchar('+');
+		_putchar ('+');
 		return (1);
 	}
-	else if( n == 0)
+	else if (n == 0)
 	{
-		_putchar('0');
+		_putchar ('0');
 		return (0);
 	}
 	else
 	{
-		_putchar( '-');
+		_putchar ('-');
 		return (-1);
 	}
 }
