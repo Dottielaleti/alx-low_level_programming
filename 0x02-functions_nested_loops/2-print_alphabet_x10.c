1477716eb54ff06fe00680ec3af96b86dba7c4d7
Dottielaleti <laletidottie@gmail.com>
2023-02-22 03:05:27 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,11 +11,10 @@ void print_alphabet_x10(void)
 	char c;
 	
 	while (i <= 9)
-		
-		for (c = 'a'; c <= 'z'; c++)
-		{
-			_putchar (c);
-		}
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar (c);
+	}
 	_putchar ('\n');
 	i++;
 }
