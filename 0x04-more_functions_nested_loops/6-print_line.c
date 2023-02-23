2118eb8b4ed84c7fa83e8f480e0a31765e4d9e61
Dottielaleti <laletidottie@gmail.com>
2023-02-23 11:32:16 -0800

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,19 +7,24 @@
   */
 void print_line(int n)
 {
+	int i;
+	int j;
+
 	if (n <= 0)
 	{
-		_putchar (\n);
+		_putchar('\n');
 	}
 	else
 	{
-		int i;
-
 		for (i = 0; i < n; i++)
 		{
-			_putchar ('_');
+			for (j = 0; j < slash; ++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
 		}
-		_putchar ('\n');
-	}
 
+	}
 }
