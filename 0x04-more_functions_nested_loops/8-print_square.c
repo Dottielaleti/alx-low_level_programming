cedae2efbfb1f8f52f9dabfe611b5a8cf786ec1c
Dottielaleti <laletidottie@gmail.com>
2023-02-23 12:25:42 -0800

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -13,14 +13,14 @@ void print_square(int size)
 
 	if (size <= 0)
 	{
-		_putchar ('\n');
+		_putchar('\n');
 	} else
 	{
 		for (i = 0; i < size; i++)
 		{
 			for (j = 0; j < size; j++)
 			{
-				-putchar('#');
+				_putchar('#');
 			}
 			_putchar('\n');
 		}
