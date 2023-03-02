2932ebce63ab3ea202f157c37ee58b52e6f4b113
Dottielaleti <laletidottie@gmail.com>
2023-03-02 10:41:46 -0800

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,18 +10,16 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
+int i;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
+	for (i = 0; i < n && *(src + i); i++)
 	{
-		dest[i] = src[i];
-		{
-			while (i < n)
-			{
-				dest[i] = '\0';
-				i++;
-			}
-			return (dest);
-		}
+		*(dest + i) = *(src + i);
 	}
-}
+	for (; i < n; i++)
+	{
+	*(dest + i) = '\0';
+	}
+	return (dest);
+
+}	
