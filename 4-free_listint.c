693faa63018ee441fb90eac2d9b2efab25012f1a
Dottielaleti <laletidottie@gmail.com>
2023-04-04 05:04:23 -0700

    4-free_listint.c

--- 4-free_listint.c
+++ 4-free_listint.c
@@ -4,16 +4,16 @@
 /**
  * free_listint - frees a listint_t list
  * @head: head of the list
- * Return: NULL if error
+ * Return: void
  */
 void free_listint(listint_t *head)
 {
-	listint_t *next;
+	listint_t *temp;
 
 	while (head != NULL)
 	{
-		next = head->next;
-		free(head);
-		head = next;
+		temp = head;
+		head = head->next;
+		free(temp);
 	}
 }
