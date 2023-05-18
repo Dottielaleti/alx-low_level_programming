be0fd72664fa558984cfabd70062365e4fcc0b7b
Dottielaleti <laletidottie@gmail.com>
2023-05-18 13:21:58 -0700

    0-print_dlistint.c

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * dlistint_len -  returns the number of elements in a linked dlistint_t list
+ * @h: name of the list
+ * Return: the number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h);
+ const dlistint_t *current = h;
+    size_t count = 0;
+
+    while (current != NULL) {
+        printf("%d\n", current->n);
+        current = current->next;
+        count++;
+    }
+
+    return count;
+}
